696b5efe74776d98212025dfe490f2271ed84160
smorrell <smorrell@rogers.com>
2022-11-18 10:22:04 -0500

    Initial checkin

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Drawing.Imaging;
+
+var sleep = (int)TimeSpan.FromMinutes(5).TotalMilliseconds;
+var tempPath = Path.GetTempPath();
+while (true)
+{
+    var image = ScreenCapture.CaptureDesktop();
+
+    var fileName = $@"{tempPath}\{DateTime.Now:yyyy-MM-dd-HH-mm-ss}.jpg";
+    image.Save(fileName, ImageFormat.Jpeg);
+    Console.WriteLine(fileName);
+    Thread.Sleep(sleep);
+}
+
